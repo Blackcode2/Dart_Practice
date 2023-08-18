960a83159a8212929cd6ed7d696d5f9092452037
Blackcode2 <72853950+Blackcode2@users.noreply.github.com>
2023-08-18 14:53:23 +0900

    Update customer.dart

--- CRUD_dart/CRUD_linked list/customer.dart
+++ CRUD_dart/CRUD_linked list/customer.dart
@@ -1,21 +1,13 @@
 import 'dart:collection';
 
-class Customer extends LinkedListEntry<Customer> {
-    String id;
-    String name;
-    String age;
+final class Customer extends LinkedListEntry<Customer> {
+  String id;
+  String name;
+  String age;
 
-    Customer(this.id, this.name, this.age);
+  Customer(this.id, this.name, this.age);
 
-    String toString() {
-        return '$id\t\t$name\t\t$age';
-    }
+  String toString() {
+    return '$id\t\t$name\t\t$age';
+  }
 }
-
-// void main() {
-//     final linkedList = new LinkedList<Customer>();
-//     linkedList.addAll(
-//         [Customer('1', 'kim', '23'), Customer('2', 'lee', '34')]
-//     );
-//     print(linkedList.first);
-// }
